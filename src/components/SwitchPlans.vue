6cabf2115f37108717a856a397e8e56d3b794536
Oana <oana.sasaran@yahoo.com>
2023-07-06 22:20:21 +0300

    added a new component

---
+++ src/components/SwitchPlans.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label class="switch">
+      <input type="checkbox" @click="toggleCheckbox" />
+      <div class="slider round"></div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.toggle-button {
+  @apply w-16 h-16 bg-orange;
+}
+</style>
